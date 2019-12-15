a4618eb36981ebfa678e27190c7409f126891357
Fiplo <pauliuz95@gmail.com>
2019-12-15 17:07:43 +0200

    Properly importing head and made initial stuff for quiz creation

---
+++ app/models/test.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+
+var questionSchema = mongoose.Schema({
+  question : String,
+  answers : [String],
+  correctAnswer: String
+});
+
+var testSchema = mongoose.Schema({
+  originalname: String,
+  madeBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  dateCreated: Date,
+  questions = [questionSchema]
+});
+
+module.exports = mongoose.model("Test", testSchema);
