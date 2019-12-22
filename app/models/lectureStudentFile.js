586b909b6de9d0c0cdbd432bcadf90d0d9aa1760
Fiplo <pauliuz95@gmail.com>
2019-12-23 00:24:04 +0200

    Changed studentFile to track status and lecturer

--- app/models/lectureStudentFile.js
+++ app/models/lectureStudentFile.js
@@ -1,11 +1,18 @@
 var mongoose = require("mongoose");
 
 var postSchema = mongoose.Schema({
-    student: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  student: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  lecturer: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 
-    filename: String,
-    filepath: String,
-    created_at: Date,
+  status: {
+    type: String,
+    enum: ["tobeuploaded", "uploaded", "checked"],
+    default: "tobeuploaded"
+  },
+
+  filename: String,
+  filepath: String,
+  created_at: Date
 });
 
 module.exports = mongoose.model("LectureStudentFile", postSchema);
